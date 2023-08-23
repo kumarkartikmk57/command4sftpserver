8428704f8d72ea57b3a20c231223c7a2a2b8394c
Kartik Kumar <kumarkartikmk57@gmail.com>
2023-08-23 14:56:03 +0000

    commands.sh created online with Bitbucket

---
+++ commands.sh
@@ -0,0 +1,26 @@
+systemctl stop firewalld
+rpm -qa|grep ssh
+mkdir -p /data/sftp
+chmod 701 /data
+groupadd sftpusers
+useradd -g sftpusers -d /upload -s /sbin/nologin nisha
+passwd nisha
+mkdir -p /data/nisha/upload
+chown -R root:sftpusers /data/nisha
+chown -R nisha:sftpusers /data/nisha/upload
+cat /etc/passwd|grep nisha
+vim /etc/ssh/sshd_config
+
+Match Group sftpusers
+ChrootDirectory /data/%u
+ForceCommand internal-sftp
+
+
+
+
+
+service sshd restart
+yum install nmap -y
+
+nmap -n 35.200.187.64
+sftp nisha@35.200.187.64
